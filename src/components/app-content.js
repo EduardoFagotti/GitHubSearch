64b5e80459aa0e3f2189da99d530c9aab27e56fa
EduardoFagotti <fagotti.edu567@outlook.com>
2022-02-07 22:42:42 -0300

    test

---
+++ src/components/app-content.js
@@ -0,0 +1,33 @@
+'use strict'
+import React from 'react'
+import PropTypes from 'prop-types'
+import Search from './search'
+import UserInfo from './user-info'
+import Actions from './actions'
+import Repos from './repos'
+
+const AppContent = ({ userinfo, repos, starred,isFetching, handleSearch, getRepos, getStarred }) => (
+  
+    <div className="app">
+    <div className="Search"> <Search handleSearch={handleSearch} /></div>
+    {isFetching && <div className="dw">Carregando...</div>}
+    {!!userinfo && <UserInfo  userInfo={userinfo} />}
+    {!!userinfo && <Actions getRepos={getRepos} getStarred={getStarred} />}
+
+    {!!repos.length && (
+      <Repos className="repos" title="Repositorios:" repos={repos} />
+    )}
+
+    {!!starred.length && (
+      <Repos className="starred" title="Favoritos:" repos={starred} />
+    )}
+  </div>
+  
+)
+AppContent.propTypes = {
+  userInfo: PropTypes.object,
+  repos: PropTypes.array.isRequired,
+  starred: PropTypes.array.isRequired,
+}
+
+export default AppContent
---
+++ src/components/search.js
@@ -0,0 +1,18 @@
+'use strict'
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Search = ({ handleSearch }) => (
+  <div className="search">
+    <input className="pq"
+      type="search"
+      placeholder=" Usuario GitHub... "
+      onKeyUp={handleSearch}
+    />
+  </div>
+)
+Search.propTypes = {
+  handleSearch: PropTypes.func.isRequired,
+}
+export default Search
